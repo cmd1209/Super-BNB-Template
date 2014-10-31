a5fe2540d31b6603fd929507193f84e51e8168da
Chad Danford <chad.m.danford@gmail.com>
2014-10-31 12:50:11 +0100

    slideshow in hero information

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,13 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $(function(){  $('.hero > div:gt(0)').hide();
+  setInterval(function(){
+    var rand = Math.floor(Math.random() * ($('.hero').children().length-1));
+    $('.hero > :first-child').appendTo('.hero').fadeOut(3250);
+    $('.hero > *').eq(rand).prependTo('.hero').fadeIn(3250);
+  }, 8000);
+});
+});
