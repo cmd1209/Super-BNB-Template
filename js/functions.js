c38514b8f80473723a0dca0fb363a23bd99f3ae2
Chad Danford <chad.m.danford@gmail.com>
2014-10-31 12:16:16 +0100

    great progress on this thing

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
 
   var slideWidth = $('#mobilewrap').outerWidth();
+  var activatorWidth = $('.mobileactivator').outerWidth();
   var activator = $('.mobileactivator');
   var mobilewrap = $('#mobilewrap');
-  var contentwrap = $('.contentwrap');
+
+  mobilewrap.css({left: -slideWidth-10});
 
   activator.click(function() {
     if (mobilewrap.is(':visible')) {
-      mobilewrap.animate({left: '-240px'},200);
+      mobilewrap.animate({left: -slideWidth-10},200);
       activator.animate({left: '5px'},200);
       activator.css({position: 'absolute'});
       mobilewrap.delay(200).fadeOut();
     }
     else{
       mobilewrap.show().animate({left: '0'},200);
-      activator.animate({left: '185px'},200);
+      activator.animate({left: slideWidth-activatorWidth},200);
       activator.css({position: 'fixed'});
     }
   });
