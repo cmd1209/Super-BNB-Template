c57780d11e2b9cc4d406d1690408974fc88cef54
Chad Danford <chad.m.danford@gmail.com>
2014-12-05 12:28:23 +0100

    Everything Uses Grunt Now

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['**/*.sass','**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            },
+        },
+
+        uglify: {
+            build: {
+                src: 'js/functions.js',
+                dest: 'js/min/functions.min.js'
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css2/style-cmd.css': 'css2/style-cmd.sass'
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['uglify','sass',]);
+
+};
